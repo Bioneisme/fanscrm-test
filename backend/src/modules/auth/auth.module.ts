e417f333a3f5734ae23df403f1430e640ae5dc9a
bioneisme <bioneisme@gmail.com>
2024-08-15 18:33:05 +0500

    Added Docker, fixed type errors

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [PassportModule, UsersModule],
+  imports: [UsersModule],
   providers: [AuthService],
   controllers: [AuthController],
 })
